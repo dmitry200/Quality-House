b172ccfe29efd3dd9ec81b9ece87c4476d134ab2
Sergey <pmswga@gmail.com>
2017-02-06 13:19:34 +0300

    Создание таблицы
    - Районы
    - ЖК
    - Район-ЖК

--- sql/qh.sql
+++ sql/qh.sql
@@ -8,6 +8,24 @@ CREATE TABLE `areas` (
   name CHAR(255) NOT NULL
 ) ENGINE = InnoDB CHARACTER SET = UTF8;
 
+CREATE TABLE `rcs` (
+  id_rc INT(11) AUTO_INCREMENT PRIMARY KEY,
+  status INT(11) NOT NULL,
+  id_builder INT(11) NOT NULL,
+  name CHAR(255) NOT NULL,
+  address CHAR(255) NOT NULL,
+  UNIQUE(name),
+  INDEX(id_rc),
+  INDEX(id_builder)
+) ENGINE = InnoDB CHARACTER SET = UTF8;
+
+CREATE TABLE `area_rc` (
+  id_rc INT(11) NOT NULL,
+  id_area INT(11) NOT NULL,
+  INDEX(id_area),
+  INDEX(id_rc),
+  PRIMARY KEY(id_rc, id_area)
+) ENGINE = InnoDB CHARACTER SET = UTF8;
 
 
 
@@ -15,6 +33,8 @@ CREATE TABLE `areas` (
 
 
 
+/* Связка таблицы "areas" с таблицей "area_rc" */
+ALTER TABLE `areas` ADD CONSTRAINT area_rc FOREIGN KEY(`id_area`) REFERENCES `area_rc` (`id_area`) ON UPDATE CASCADE;
 
-/* Связка таблицы "" с таблицей "" 
-ALTER TABLE `answers` ADD CONSTRAINT question_answers FOREIGN KEY(`id_question`) REFERENCES `questions` (`id_question`) ON UPDATE CASCADE ON DELETE CASCADE;*/
+/* Связка таблицы "areas" с таблицей "area_rc" */
+ALTER TABLE `rcs` ADD CONSTRAINT rc_area FOREIGN KEY(`id_rc`) REFERENCES `area_rc` (`id_rc`) ON UPDATE CASCADE;
