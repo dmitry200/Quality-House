4a145c652d7aa675b7f34b81cdc9a66750247640
Sergey <pmswga@gmail.com>
2017-02-06 15:19:37 +0300

    Скрипт по созданию таблиц

--- sql/qh.sql
+++ sql/qh.sql
@@ -45,6 +45,38 @@ CREATE TABLE `builders` (
   UNIQUE(name)
 ) ENGINE = InnoDB CHARACTER SET = UTF8;
 
+CREATE TABLE `rc_home` (
+  id_rc INT(11) NOT NULL,
+  id_home INT(11) NOT NULL,
+  INDEX(id_rc),
+  INDEX(id_home),
+  PRIMARY KEY(id_rc, id_home)
+) ENGINE = InnoDB CHARACTER SET = UTF8;
+
+CREATE TABLE `homes` (
+  id_home INT(11) PRIMARY KEY,
+  count_floors INT(11) NOT NULL,
+  count_proch INT(11) NOT NULL,
+  INDEX(id_home)
+) ENGINE = InnoDB CHARACTER SET = UTF8;
+
+CREATE TABLE `flats` (
+  id_flat INT(11) PRIMARY KEY,
+  count_rooms INT(11) NOT NULL,
+  square INT(11) NOT NULL,
+  balcony BOOLEAN NOT NULL,
+  repair CHAR(255) NOT NULL,
+  INDEX(id_flat)
+) ENGINE = InnoDB CHARACTER SET = UTF8;
+
+CREATE TABLE `home_flat` (
+  id_home INT(11) NOT NULL,
+  id_flat INT(11) NOT NULL,
+  price_flat INT(11) NOT NULL,
+  porch INT(11) NOT NULL,
+  floor INT(11) NOT NULL,
+  PRIMARY KEY(id_home, id_flat)
+) ENGINE = InnoDB CHARACTER SET = UTF8;
 
 /* Связка таблицы "area_rc" с таблицей "areas" */
 ALTER TABLE `area_rc` ADD CONSTRAINT area_rc FOREIGN KEY(`id_area`) REFERENCES `areas` (`id_area`) ON UPDATE CASCADE;
@@ -57,3 +89,15 @@ ALTER TABLE `rcs` ADD CONSTRAINT rc_builder FOREIGN KEY(`id_builder`) REFERENCES
 
 /* Связка таблицы "rc_status" с таблицей "rcs" */
 ALTER TABLE `rcs` ADD CONSTRAINT rc_stat FOREIGN KEY(`status`) REFERENCES `rc_status` (`id_status`) ON UPDATE CASCADE;
+
+/* Связка таблицы "rc" с таблицей "rc_home" */
+ALTER TABLE `rc_home` ADD CONSTRAINT rc_home FOREIGN KEY(`id_rc`) REFERENCES `rcs` (`id_rc`) ON UPDATE CASCADE;
+
+/* Связка таблицы "home" с таблицей "rc_home" */
+ALTER TABLE `rc_home` ADD CONSTRAINT home_rc FOREIGN KEY(`id_home`) REFERENCES `homes` (`id_home`) ON UPDATE CASCADE;
+
+/* Связка таблицы "flat" с таблицей "home_flat" */
+ALTER TABLE `home_flat` ADD CONSTRAINT home_flat FOREIGN KEY(`id_flat`) REFERENCES `flats` (`id_flat`) ON UPDATE CASCADE;
+
+/* Связка таблицы "home" с таблицей "home_flat" */
+ALTER TABLE `home_flat` ADD CONSTRAINT flat_home FOREIGN KEY(`id_home`) REFERENCES `homes` (`id_home`) ON UPDATE CASCADE;
